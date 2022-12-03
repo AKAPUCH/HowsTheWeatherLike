8c995c0be18c87377e5f7d1f458c4edc3ca04dd6
Beemo97 <chldnxo5@naver.com>
2022-12-03 20:43:22 +0900

    Initial Commit

---
+++ HowsTheWeatherLike/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HowsTheWeatherLike
+//
+//  Created by 최우태 on 2022/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
