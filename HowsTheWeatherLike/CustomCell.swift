b47cc8bb5c6a3a274a1d587afcebdd0406c46334
Beemo97 <chldnxo5@naver.com>
2022-12-04 18:20:22 +0900

    get JSON

---
+++ HowsTheWeatherLike/CustomCell.swift
@@ -0,0 +1,44 @@
+//
+//  CustomCell.swift
+//  HowsTheWeatherLike
+//
+//  Created by 최우태 on 2022/12/04.
+//
+
+import UIKit
+
+class CustomCell : UITableViewCell {
+    
+    static let identifier = "CustomCell"
+    
+    lazy var leftImageView : UIImageView  = {
+       let imageView = UIImageView()
+        return imageView
+    }()
+    
+    let label : UILabel = {
+       let label = UILabel()
+        label.textAlignment = .left
+        return label
+    }()
+    
+    override init(style : UITableViewCell.CellStyle, reuseIdentifier : String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension CustomCell {
+    func bind(model : NationWeatherM) {
+        
+        label.text = model.korean_name
+        
+        let imageAsset : UIImage = UIImage(systemName: "(\(model.asset_name))")!
+        leftImageView.image = imageAsset
+    }
+}
--- HowsTheWeatherLike/ViewController.swift
+++ HowsTheWeatherLike/ViewController.swift
@@ -8,12 +8,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //변수 정의
+    
+    var countries : [NationWeatherM] = []
+    var weatherDetail : [NationWeatherD] = []
+    
+    lazy var table : UITableView = {
+       let tableSettings = UITableView()
 
+        return tableSettings
+    }()
+    
+    
+    
+    //functions
+    
+    func tableSetup() {
+        table.register(CustomCell.self, forCellReuseIdentifier: CustomCell.identifier)
+        table.delegate = self
+        table.dataSource = self
+    }
+    
+    func getJsonData() {
+        let jsonDecoder : JSONDecoder = JSONDecoder()
+        
+        guard let dataAsset1 : NSDataAsset = NSDataAsset.init(name: "countries" ) else {return}
+        
+        do{
+            self.countries = try jsonDecoder.decode([NationWeatherM].self, from: dataAsset1.data)
+        }catch {
+            print(error.localizedDescription)
+        }
+        self.table.reloadData()
+    }
+    
+    //override functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableSetup()
+    }
+    
+    override func loadView() {
+        view.backgroundColor = .white
+        view.addSubview(table)
+        tableSetup()
+        getJsonData()
     }
 
 
 }
+
+//extension(delegate)
+
+extension ViewController : UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.identifier) as? CustomCell ?? CustomCell()
+        
+        cell.bind(model: countries[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    
+}
+
 
